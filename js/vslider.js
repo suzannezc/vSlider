4d24f2944121226673cd43156f082da45660141f
suzannezc <suzanne@synapticimpulse.com>
2014-02-24 15:06:35 -0500

    Default Values to Match General Settings
    + adjusted default values

--- js/vslider.js
+++ js/vslider.js
@@ -571,13 +571,13 @@
 	
 	// default values
 	$.fn.coinslider.defaults = {	
-		width: 630, // width of slider panel
-		height: 280, // height of slider panel
+		width: 600, // width of slider panel
+		height: 210, // height of slider panel
 		spw: 7, // squares per width
 		sph: 5, // squares per height
 		delay: 3000, // delay between images in ms
 		sDelay: 30, // delay beetwen squares in ms
-		opacity: 0.7, // opacity of title and navigation
+		opacity: 0.5, // opacity of title and navigation
 		titleSpeed: 500, // speed of title appereance in ms
 		effect: '', // random, swirl, rain, straight
 		navigation: true, // prev next and buttons
